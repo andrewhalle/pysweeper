19c0f298a8a08db7943ac26c8d1b9f354a97f007
Andrew Halle <ahalle@berkeley.edu>
2018-10-25 12:34:06 -0700

    change file structure

--- pysweeper/game.py
+++ pysweeper/game.py
@@ -138,12 +138,3 @@ class Minesweeper:
             sys.stdout.write("\n")
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="play minesweeper!")
-    parser.add_argument("-n", "--size", action="store",
-            type=int, default=10, help="board size")
-    args = parser.parse_args()
-    game = Minesweeper(size=args.size)
-    while game.play_again:
-        game.reset()
-        game.play()
