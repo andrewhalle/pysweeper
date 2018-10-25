e0f8fa4a29e6a6d8956b2e4f0f96ffc3215370c1
Andrew Halle <ahalle@berkeley.edu>
2018-10-25 12:48:02 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pysweeper',
+    version='0.1.0',
+    author='Andrew Halle',
+    author_email='ahalle@berkeley.edu',
+    packages=["pysweeper"],
+    scripts=["main.py"],
+    url='andrewhalle.com',
+    description='minesweeper',
+)
